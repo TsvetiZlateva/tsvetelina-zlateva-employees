db040cb10d4c79a7c7aa93231ae6099d3c3d8500
Tsveti Zlateva <tsveti.zlateva@gmail.com>
2021-06-14 17:14:29 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tsvetelina_zlateva_employees.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
